028e41d8567fc5847db94f033ec92379d7eaab82
pasham Revanth <pashamrevanth541@gmail.com>
2025-12-23 01:31:17 +0530

    updated the url icon and text

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,17 @@ import "./globals.css"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/Footer"
 
+export const metadata = {
+  title: "Revanth – Mern Stack Web Developer",
+  description: "Portfolio of Revanth, Mern stack web developer",
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-background text-white">
         <Navbar />
-
-        {/* Page Content */}
-        <main className="min-h-screen">
-          {children}
-        </main>
-
+        <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
